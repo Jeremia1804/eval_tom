350226d4f0b6d1b865cb5ba6cf26ac875f482c2c
andraina1901 <randraina@yahoo.com>
2024-06-03 01:43:20 +0300

    classement

--- projet/controllers/client_liantsoa.py
+++ projet/controllers/client_liantsoa.py
@@ -10,3 +10,20 @@ def index():
 def login_equipe():
     return render_template("login-equipe.html")
 
+
+@app.route('/liste-etape', methods = ['GET'])
+def liste_etape():
+    return render_template("client/list-etape.html")
+
+@app.route('/coureur-etape', methods =['GET'])
+def coureur_etape():
+    return render_template("client/coureur-etape.html")
+
+@app.route('/classement-equipe' , methods = ['GET'])
+def classment_eq_cl():
+    return render_template("client/classement-equipe.html")
+
+@app.route('/classement-etape' , methods = ['GET'])
+def classment_etape_cl():
+    return render_template("client/classement-etape.html")
+
